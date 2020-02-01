27c95534038a5985aac3cc2fe63f0ceeb72bbcec
Rony Portelli <portelli.rony@gmail.com>
2020-02-01 21:58:41 +0000

    the grand hack

--- Assets/Scripts/SyncCommands.cs
+++ Assets/Scripts/SyncCommands.cs
@@ -6,31 +6,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SyncCommands : MonoBehaviourPun
+public class SyncCommands : MonoBehaviour
 {
-    public void OnEnable()
-    {
-        PhotonNetwork.NetworkingClient.EventReceived += Yee;
-    }
-
-    public void OnDisable()
-    {
-        PhotonNetwork.NetworkingClient.EventReceived -= Yee;
-    }
-
-    private void Yee(EventData obj)
-    {
-        Debug.Log("It works");
-    }
-
-    
     public GameObject target;
 
     public void CallToggleTarget()
     {
         Debug.Log("The call of the wild");
-        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All};
-        SendOptions sendOptions = new SendOptions { Reliability = true };
-        PhotonNetwork.RaiseEvent(0, null, raiseEventOptions, sendOptions);
+        PhotonNetwork.GetPhotonView(99).RPC("TheGrandHack",RpcTarget.All);
     }
-}
+
+    [PunRPC]
+    public void TheGrandHack() 
+    {
+        Debug.Log("Event Blyat no, RPC!!!!");
+        if(target != null)
+        {
+            target.SetActive(!target.activeSelf);
+        }
+    }
+    }
