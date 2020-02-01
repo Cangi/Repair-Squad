9a1789b80f4255c1b4f793abb7469a1e83d14e32
Rony Portelli <portelli.rony@gmail.com>
2020-02-01 19:00:16 +0000

    Added join session script

---
+++ Assets/Scripts/JoinSession.cs
@@ -0,0 +1,125 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class JoinSession : MonoBehaviourPunCallbacks, ILobbyCallbacks
+{
+    public PhotonView sparkyView;
+    public PhotonView plumberView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExitGames.Client.Photon.Hashtable hash = new ExitGames.Client.Photon.Hashtable();
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "HoloLens":
+                hash.Add("Type","HoloLens");
+                break;
+            case "Sparky":
+                hash.Add("Type", "Sparky";
+                break;
+            case "Plumber":
+                hash.Add("Type", "Plumber");
+                break;
+        }
+        
+        ConnectNow();
+    }
+
+    public void ConnectNow()
+    {
+        Debug.Log("ConnectAndJoinRandom.ConnectNow() will now call: PhotonNetwork.ConnectUsingSettings().");
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = "1.0";
+    }
+
+    public void Disconect()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void OnJoinedLobby()
+    {
+        if (PhotonNetwork.CountOfRooms == 0)
+        {
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = 0;
+            PhotonNetwork.CreateRoom("RepairSquadSession", roomOptions, PhotonNetwork.CurrentLobby);
+            Debug.Log("Created session");
+        }
+        else
+        {
+            PhotonNetwork.GetCustomRoomList(TypedLobby.Default, "");
+            PhotonNetwork.JoinRoom("RepairSquadSession");
+            Debug.Log("Joining session");
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Just joined: " + PhotonNetwork.CurrentRoom.Name);
+        switch(SceneManager.GetActiveScene().name)
+        {
+            case "HoloLens":
+                GetComponent<PhotonView>().ViewID = 1;
+                break;
+            case "Sparky":
+                GetComponent<PhotonView>().ViewID = 2;
+                break;
+            case "Plumber":
+                GetComponent<PhotonView>().ViewID = 3;
+                break;
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.ActorNumber + " joined the room");
+        switch(newPlayer.CustomProperties["Type"])
+        {
+            case "HoloLens":
+                break;
+            case "Sparky":
+                sparkyView.ViewID = 2;
+                break;
+            case "Plumber":
+                plumberView.ViewID = 3;
+                break;
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log(otherPlayer.ActorNumber + " left the room");
+        photonViews.Push(PhotonNetwork.GetPhotonView(otherPlayer.ActorNumber));
+    }
+
+    public void OnLeftLobby()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnLobbyStatisticsUpdate(List<TypedLobbyInfo> lobbyStatistics)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo x in roomList)
+        {
+            Debug.Log(x.Name);
+            Debug.Log(x.PlayerCount);
+        }
+    }
+
+}
